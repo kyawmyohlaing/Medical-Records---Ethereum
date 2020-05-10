28855f9ca75ea0f47bc6fe2dabed0701772d66be
kyawmyohlaing <kyawmyohlaingygn4@gmail.com>
2020-05-10 14:23:11 +0630

    Create .env

--- Medical/truffle-config.js
+++ Medical/truffle-config.js
@@ -18,9 +18,9 @@ module.exports = {
     ropsten: {
     //  host: "127.0.0.1",
     //  port: 8545,
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+infura_apikey)
-      },
+    //  provider: function() {
+    //    return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+infura_apikey)
+    //  },
       network_id: 3,
       skipDryRun: true, // I don't know why I had to do this but would not deploy until I disabled simulation run
       gas:4700000
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Medical-Records---Ethereum
 This project aims to develop and implement an application that will secure patients medical records based on the ethereum blockchain.
+
+#npm install
+#npm install --save truffle-hdwallet-provider
+#npm install --save truffle-contract
